8c69fb2dd0dda0f146eef105c7b598e167b1b031
Yash8452 <yashgharat1999@gmail.com>
2024-03-11 02:45:58 +0530

    badge for categories

---
+++ src/app/components/Badge.tsx
@@ -0,0 +1,21 @@
+export function Badge() {
+  return (
+    <div className="mt-16 fixed z-50 mb-5 flex space-x-2">
+      <span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+        frontend
+      </span>
+      <span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+        backend
+      </span>
+      <span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+        javascript
+      </span>
+      <span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+        nextjs
+      </span>
+      <span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+        nodejs
+      </span>
+    </div>
+  );
+}
